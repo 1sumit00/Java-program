3aa4a070b4fb9191ff371956de63cd7efddabb73
1sumit00 <115863536+1sumit00@users.noreply.github.com>
2024-09-19 13:43:45 +0530

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,16 @@
+class main{
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+        System.out.print("******");
+    }
+  }
+
+
+
+// Language:Java 
+// ProjectType:0 
+// Copy the full code and open the JavaCoder APP to run it. 
+// JavaCoder APP download link：https://play.google.com/store/apps/details?id=com.ikou.jcoding 
